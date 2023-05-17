a48185d3016aa489f8c0abfe07ad5fa212350801
Rashid <raabdulk@gmail.com>
2023-05-17 23:25:29 +0300

    Home work 6

---
+++ js/hw-6.js
@@ -0,0 +1,109 @@
+// Задание 1
+
+// let numbers = [1, 5, 4, 10, 0, 3];
+
+// for (let i = 0; i < numbers.length; i++) {
+//     console.log(numbers[i]);
+//     if (numbers[i] == 10) break;
+// }
+
+// Задание 2
+
+// const numbs = [1, 5, 4, 10, 0, 3];
+// console.log(numbs.indexOf(4));
+
+// Задание 3
+
+// let numbs = [1, 3, 5, 10, 20];
+// numbs = numbs.join(' ');
+// console.log(numbs);
+
+// Задание 4
+
+// let arr = [
+//     [1, 1, 1], 
+//     [1, 1, 1], 
+//     [1, 1, 1],
+
+// ];
+
+// Задание 5
+
+// let arr =  [1, 1, 1];
+
+// arr.push('2, 2, 2');
+
+// Задание 6
+
+// let arr = [9, 8, 7, 'a', 6, 5];
+
+// arr.sort();
+// console.log(arr);
+// arr.pop();
+// console.log(arr);
+
+// Задание 7
+
+// let arr = [9, 8, 7, 6, 5];
+// let numbers = Number(prompt('Введите число от 0 до 10'));
+// console.log(numbers);
+
+// let search = arr.includes(numbers);
+// console.log(search);
+
+// Задание 8
+
+// const words = 'abcdef'
+// const arr = words.split('')
+// console.log(arr)
+
+// let arr2 = arr.reverse()
+// console.log(arr2)
+
+// arr2 = arr2.join('')
+// console.log(arr2)
+
+// Задание 9
+
+// let arr = [
+//     [1, 2, 3],
+//     [4, 5, 6],
+// ];
+
+// const merge = arr.flat(1);
+// console.log(merge)
+
+// Задание 10
+
+// let arr = [2, 5, 8, 3, 1, 2, 7];
+
+// for (let i = 0; i < arr.length - 1; i++) {
+//     console.log(arr[i] + arr[i + 1]);
+// }
+
+// Задание 11
+
+// const numbs = [1, 2, 3, 4, 5];
+
+// const squares = numbs.map(function (num) {
+//     return num * num
+// })
+
+// console.log(squares)
+
+// Задание 12
+
+// const getLengthWords = ['слово', '', 'слог', 'длинное предложение', 'буква'];
+
+// let num = getLengthWords.map((far) => {
+//     return far.length;
+// })
+// console.log(num)
+
+
+// Задание 13
+
+// const myArray = [-25, 25, 0, -1000, -2];
+
+// let negatives = myArray.filter(e => e < 0);
+// console.log(negatives);
